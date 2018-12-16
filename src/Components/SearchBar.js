3718be9a8258a3d41156a2a45be5a589f48a006a
Jeremiah Ogutu <jogutu@pm.me>
2018-12-16 15:51:51 -0500

    added searchbar input field and label

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    state = {
+        searchTerm: ''
+    };
     render() {
-        return <div>SearchBar</div>
+        return <div className='search-bar ui segment'>
+            <form className='ui form'>
+                <div className='field'>
+                    <label>Video Search</label>
+                    <input
+                        type="text"
+                        value={this.state.searchTerm}
+                    />
+                </div>
+            </form>
+        </div>
     }
 }
 
