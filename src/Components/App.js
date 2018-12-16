1475cfe0973104826f1975b91a57ffca110ecb39
Jeremiah Ogutu <jogutu@pm.me>
2018-12-16 15:33:29 -0500

    added searchBar component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
+import SearchBar from "./SearchBar";
 
 class App extends Component {
     render() {
-        return <div>App</div>
+        return (
+            <div><SearchBar/></div>
+        )
     }
 }
 
