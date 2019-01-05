cabc29ad3c4319d7a8143d4c277030e9d1d33711
Jeremiah Ogutu <jogutu@pm.me>
2019-01-05 17:04:58 -0500

    added grid and fixed warnings

--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoItem from "./VideoItem";
 
 const VideoList = ({videos, onVideoSelect }) => {
-    const renderedList = videos.map(video => <VideoItem onVideoSelect={onVideoSelect} video={video}/>);
+    const renderedList = videos.map(video => <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>);
     return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
